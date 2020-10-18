64c56347feed3fc25054aed07bd87b4ab5e8297c
Andrew20Ayman <andrew13ayman@gmail.com>
2020-10-18 14:35:48 +0200

    Auto-generated commit

--- src/app/core/models/Register.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Register {
-    name: string;
-    email: string;
-    username : string;
-    password : string;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  api_url: "https://private-b2e6827-robustatask.apiary-mock.com"
-};
